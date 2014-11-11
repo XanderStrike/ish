08c5700c0e492db6757820561324fcc213565b1c
Alexander Standke <xanderstrike@gmail.com>
2014-11-11 15:20:03 -0800

    add ip addresses

--- i.sh
+++ i.sh
@@ -6,3 +6,6 @@ free -h | grep Mem | awk '{print "RAM: " $3 " used of " $2 " (" $7 " cached)"}'
 df -h | grep sda1 | awk '{print "Root: " $3 " used of " $2 " (" $5 ")"}'
 df -h | grep Private | awk '{print "Home: " $3 " used of " $2 " (" $5 ")"}'
 sensors | grep Core | awk '{print $1 " " $2 " " $3}'
+printf "IPs: "
+ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | tr '\n' " "
+printf "\n"
