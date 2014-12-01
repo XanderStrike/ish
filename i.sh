66eb9b3294d48ccf1336ba316acd75b1940a3c9a
Alexander Standke <xanderstrike@gmail.com>
2014-11-30 22:54:02 -0800

    added a new hard drive

--- i.sh
+++ i.sh
@@ -3,8 +3,9 @@
 uptime -p
 cat /proc/loadavg | awk '{print "Load:\t" $1 " " $2 " " $3}'
 free -h | grep Mem | awk '{print "RAM:\t" $3 " used of " $2 " (" $7 " cached)"}'
-df -h | grep sda1 | awk '{print "Root:\t" $3 " used of " $2 " (" $5 ")"}'
+df -h | grep sdb1 | awk '{print "Root:\t" $3 " used of " $2 " (" $5 ")"}'
 df -h | grep Private | awk '{print "Home:\t" $3 " used of " $2 " (" $5 ")"}'
+df -h | grep sda1 | awk '{print "Store:\t" $3 " used of " $2 " (" $5 ")"}'
 sensors | grep Physical | awk '{print "Temp:\t" $4}'
 printf "IPs:\t"
 ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | tr '\n' " "
