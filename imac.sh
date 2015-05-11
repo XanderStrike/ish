67305201d10795f3ebe6710a1ad6ef03fa4f462a
Alexander Standke <xanderstrike@gmail.com>
2015-05-11 09:12:51 -0700

    change the order of things

--- imac.sh
+++ imac.sh
@@ -2,10 +2,9 @@
 
 uptime
 df -h | grep disk1 | awk '{print "Root:\t" $3 " used of " $2 " (" $5 ")"}'
-istats cpu | awk '{print "CPU:\t" $3 " " $4}'
-istats battery charge | awk '{print "Batt:\t" $3}'
-istats battery temp | awk '{print "Batt:\t" $3}'
-
 printf "IPs:\t"
 ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | tr '\n' " "
 printf "\n"
+istats cpu | awk '{print "CPU:\t" $3 " " $4}'
+istats battery charge | awk '{print "Batt:\t" $3}'
+istats battery temp | awk '{print "Batt:\t" $3}'
