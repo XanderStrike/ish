1cde425243224dcfa9d0819b7b2d00c468c081af
Alexander Standke <xanderstrike@gmail.com>
2015-05-11 09:24:29 -0700

    don't use grep when it's not necessary

--- imac.sh
+++ imac.sh
@@ -2,7 +2,7 @@
 
 uptime
 top -l 1 | head -n 10 | awk '/PhysMem:/ {print "RAM:\t" $2 " used, " $6 " unused"}'
-df -h | grep disk1 | awk '{print "Root:\t" $3 " used of " $2 " (" $5 ")"}'
+df -h | awk '/disk1/ {print "Root:\t" $3 " used of " $2 " (" $5 ")"}'
 printf "IPs:\t"
 ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | tr '\n' " "
 printf "\n"
